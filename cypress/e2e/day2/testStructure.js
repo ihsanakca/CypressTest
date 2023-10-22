03318400780e9f4a1ed2fccdfd586f44afb3739d
ihsan <ihsanakcair@gmail.com>
2023-10-23 01:26:48 +0300

    added: testStructure

---
+++ cypress/e2e/day2/testStructure.js
@@ -0,0 +1,31 @@
+describe('First Test Suit', function () {
+
+    beforeEach(()=>{
+        cy.visit('www..... url')
+    })
+
+    it('First Test Case', function () {
+        syntax
+    })
+    it('Second Test Case', function () {
+        syntax
+    })
+    it('Third Test Case', function () {
+        syntax
+    })
+
+})
+
+describe('Second Test Suit', () => {
+
+    it('First Test Case', () => {
+        syntax
+    })
+    it('Second Test Case', () => {
+        syntax
+    })
+    it('Third Test Case', () => {
+        syntax
+    })
+
+})
